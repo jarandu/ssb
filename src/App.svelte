6e01f61cc19008b6aae210585c3fcf37519571f0
Jarand Ullestad <jarand.ullestad@nationen.no>
2023-05-15 15:18:28 +0200

    Loading spinner

--- src/App.svelte
+++ src/App.svelte
@@ -103,7 +103,7 @@ onMount(async () => {
     fetch(url)
     .then(r => r.json())
     .then(d => {
-        console.log(d)
+        console.log(urlParams)
         populate(d)
     })
     .catch(e => console.log(e))
@@ -112,9 +112,20 @@ onMount(async () => {
 </script>
 
 <h2>Prisvekst siden invasjonen av Ukraina</h2>
+{#if output}
 <p class=description>Trykk på varekategoriene for å legge til eller fjerne fra diagramet.</p>
 <div class=canvas-container><Line data={output} options={options} /></div>
 <div class=credits>Kilde: SSB. Oppdatert {updated != "" ? updated.toLocaleDateString('nb-NO', { year: 'numeric', month: 'long', day: 'numeric' }) : "Kunne ikke hente tidspunkt."}. Utvikling: Jarand Ullestad/Nationen.</div>
+{:else}
+<div class=canvas-container>
+    <div class=loading>
+        <div class="spinner">
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+</div>
+{/if}
 
 <style>
 .description {
@@ -128,5 +139,38 @@ onMount(async () => {
     margin-top: 10px;
     font-size: 11px;
     color: #888;
+}
+.loading {
+    display: flex;
+    width: 100%;
+    height: 100%;
+    align-items: center;
+}
+.spinner {
+    width: 40px;
+    height: 40px;
+    position: relative;
+    margin: 100px auto;
+}
+.spinner div {
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    border: 2px solid #406619;
+    position: absolute;
+    top: 0;
+    left: 0;
+    animation: bounce 1.5s infinite ease-in-out;
+}
+.spinner div:nth-child(2) {
+    animation-delay: -1.0s;
+    border-color: #fcda51;
+}
+@keyframes bounce {
+    0%, 100% { 
+        transform: scale(0.3);
+    } 50% { 
+        transform: scale(1);
+    }
 }
 </style>
